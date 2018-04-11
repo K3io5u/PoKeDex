b155f0e0c9c218272c946bb9aa30e121a9c9e888
K3io5u <k3io5u@github.com>
2018-04-11 19:28:16 -0400

    Program gives PokeMon name based on ID inputted, currently only gen1 is implemented

--- src/dex/poke/PokeDexMain.java
+++ src/dex/poke/PokeDexMain.java
@@ -1,5 +1,32 @@
 package dex.poke;
 
+import java.util.Scanner;
+
 public class PokeDexMain {
 
+    PokeDexMain() {
+
+    }
+
+    public static void main(String[] args) {
+        int pokeMon;
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Welcome to the PoKeDex!");
+        System.out.println();
+        System.out.println("What PoKeMon would you like to learn more about? (enter PoKeDex Number)");
+        pokeMon = input.nextInt();
+
+        PokeGenOne call = new PokeGenOne();
+
+        Integer num = pokeMon;
+
+        try {
+           System.out.println(call.gen1.get(num));
+        }
+        catch(NullPointerException e) {
+           System.exit(0);
+        }
+    }
 }
---
+++ src/dex/poke/PokeMon.java
@@ -0,0 +1,8 @@
+package dex.poke;
+
+public class PokeMon extends PokeDexMain {
+
+    PokeMon() {
+        super();
+    }
+}
